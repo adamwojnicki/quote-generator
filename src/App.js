40d749856b12f0d618eac79af0a944413aadea4c
Adam Wojnicki <wojnicki.ada1@gmial.com>
2020-10-02 21:18:17 +0200

    Axios installation and random quote generation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 
 export default class App extends Component {
+  state = {
+    quote: {},
+  };
+
+  async getRandomQuote() {
+    console.log("random quote generated");
+    const res = await axios.get(
+      "https://quote-garden.herokuapp.com/api/v2/quotes/random"
+    );
+    this.setState({ quote: res.data.quote });
+  }
+
+  componentDidMount() {
+    this.getRandomQuote();
+  }
   render() {
     return (
       <div className="container">
         <header>
-          <button>random</button>
+          <button onClick={() => this.getRandomQuote()}>random</button>
         </header>
         <main>
-          <blockquote>
-            “The first rule of any technology used in a business is that
-            automation applied to an efficient operation will magnify the
-            efficiency. The second is that automation applied to an inefficient
-            operation will magnify the inefficiency.”
-          </blockquote>
+          <blockquote>{this.state.quote.quoteText}</blockquote>
           <button>
-            <p className="author">Bill Gates</p>
-            <p className="category">Business</p>
+            <p className="author">{this.state.quote.quoteAuthor}</p>
+            <p className="category">{this.state.quote.quoteGenre}</p>
           </button>
         </main>
       </div>
